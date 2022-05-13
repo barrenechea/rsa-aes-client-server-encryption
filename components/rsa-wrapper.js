ed84ef868cf240903410f051e67d2179e6d39283
Sebastián Barrenechea <sebastian@barrenechea.cl>
2022-05-13 17:16:36 -0400

    refactor: attempt to use webcrypto

--- components/rsa-wrapper.js
+++ components/rsa-wrapper.js
@@ -1,9 +1,6 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
-import crypto from 'node:crypto';
-import { promisify } from 'node:util';
-
-const generateKeyPairAsync = promisify(crypto.generateKeyPair);
+import { webcrypto } from 'node:crypto';
 
 export const initLoadServerKeys = async () => {
   const [serverPub, serverPrivate, clientPub] = await Promise.all([
@@ -12,48 +9,70 @@ export const initLoadServerKeys = async () => {
     fs.readFile(path.resolve('keys', 'client.public.pem'))
   ]);
 
+  const serverPubString = serverPub.toString('utf8').replace(/^-+(BEGIN|END) PUBLIC KEY-+\n/, '').replace(/\n/g, '');
+  const serverPrivateString = serverPrivate.toString('utf8').replace(/^-+(BEGIN|END) PRIVATE KEY-+\n/, '').replace(/\n/g, '');
+  const clientPubString = clientPub.toString('utf8').replace(/^-+(BEGIN|END) PUBLIC KEY-+\n/, '').replace(/\n/g, '');
+
+  const serverPubBuffer = Buffer.from(serverPubString, 'base64');
+  const serverPrivateBuffer = Buffer.from(serverPrivateString, 'base64');
+  const clientPubBuffer = Buffer.from(clientPubString, 'base64');
+
+  const serverPubKey = await webcrypto.subtle.importKey('spki', serverPubBuffer, { name: 'RSA-OAEP', hash: { name: 'SHA-256' } }, true, ['encrypt', 'wrapKey']);
+  const serverPrivateKey = await webcrypto.subtle.importKey('pkcs8', serverPrivateBuffer, { name: 'RSA-OAEP', hash: { name: 'SHA-256' } }, true, ['decrypt', 'unwrapKey']);
+  const clientPubKey = await webcrypto.subtle.importKey('spki', clientPubBuffer, { name: 'RSA-OAEP', hash: { name: 'SHA-256' } }, true, ['encrypt', 'wrapKey']);
+
   return {
-    serverPub,
-    serverPrivate,
-    clientPub,
+    serverPub: serverPubKey,
+    serverPrivate: serverPrivateKey,
+    clientPub: clientPubKey
   };
 };
 
 export const generateKeyPair = async (direction) => {
-  const keys = await generateKeyPairAsync('rsa', {
+  const options = {
+    name: 'RSASSA-PKCS1-v1_5',
     modulusLength: 2048,
-    publicKeyEncoding: {
-      type: 'spki',
-      format: 'pem',
-    },
-    privateKeyEncoding: {
-      type: 'pkcs8',
-      format: 'pem',
-    },
-  });
+    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+    hash: { name: 'SHA-256' },
+  };
+
+  const keys = await webcrypto.subtle.generateKey(options, true, ['sign', 'verify']);
+
+  const [publicKey, privateKey] = await Promise.all([
+    webcrypto.subtle.exportKey('spki', keys.publicKey),
+    webcrypto.subtle.exportKey('pkcs8', keys.privateKey),
+  ]);
+
+  const base64PubKey = Buffer.from(publicKey).toString('base64').match(/.{1,64}/g).join('\n');
+  const base64PrivKey = Buffer.from(privateKey).toString('base64').match(/.{1,64}/g).join('\n');
+
+  const publicKeyFile = `-----BEGIN PUBLIC KEY-----\n${base64PubKey}\n-----END PUBLIC KEY-----\n`;
+  const privateKeyFile = `-----BEGIN PRIVATE KEY-----\n${base64PrivKey}\n-----END PRIVATE KEY-----\n`;
 
   await Promise.all([
-    fs.writeFile(path.resolve('keys', `${direction}.private.pem`), keys.privateKey),
-    fs.writeFile(path.resolve('keys', `${direction}.public.pem`), keys.publicKey)
+    fs.writeFile(path.resolve('keys', `${direction}.public.pem`), publicKeyFile),
+    fs.writeFile(path.resolve('keys', `${direction}.private.pem`), privateKeyFile),
   ]);
 };
 
-export const encryptRSA = (publicKey, message) => {
+export const encryptRSA = async (publicKey, message) => {
   const decryptedBuffer = Buffer.from(message);
-  const encryptedBuffer = crypto.publicEncrypt({
-    key: publicKey,
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-  }, decryptedBuffer);
+  const encryptedBuffer = await webcrypto.subtle.encrypt({
+    name: 'RSA-OAEP',
+    publicKey,
+    hash: { name: 'SHA-256' },
+  }, publicKey, decryptedBuffer);
 
-  return encryptedBuffer.toString('base64');
+  return Buffer.from(encryptedBuffer).toString('base64');
 };
 
-export const decryptRSA = (privateKey, base64Message) => {
+export const decryptRSA = async (privateKey, base64Message) => {
   const encryptedBuffer = Buffer.from(base64Message, 'base64');
-  const decryptedBuffer = crypto.privateDecrypt({
-    key: privateKey,
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-  }, encryptedBuffer);
+  const decryptedBuffer = await webcrypto.subtle.decrypt({
+    name: 'RSA-OAEP',
+    privateKey,
+    hash: { name: 'SHA-256' },
+  }, privateKey, encryptedBuffer);
 
-  return decryptedBuffer.toString();
-};
+  return Buffer.from(decryptedBuffer).toString();
+}
